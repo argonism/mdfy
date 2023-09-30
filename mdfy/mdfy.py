9282d5c5be8e7ea131287183a36e6294eb5df252
k-usuha <kouta528bado@yahoo.co.jp>
2023-09-30 12:55:22 +0900

    Remove unused __open__ __close__

--- mdfy/mdfy.py
+++ mdfy/mdfy.py
@@ -8,12 +8,6 @@ class Mdfy:
     def __init__(self, filepath):
         self.filepath = filepath
 
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        pass
-
     def write(self, content):
         with open(self.filepath, "w") as file:
             for item in content:
