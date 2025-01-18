e8fd9464d382ffe4b4fa823eab0eb3f0d7784747
argonism <44169535+argonism@users.noreply.github.com>
2025-01-19 02:24:40 +0900

    Fix garbled characters for windows

--- tests/mdfy_test.py
+++ tests/mdfy_test.py
@@ -38,3 +38,21 @@ def test_mdfy_write_with_statement():
             assert lines[0] == "# Hello, MDFY!\n"
             assert lines[1] == "**Life** is *like* a bicycle.\n"
             assert mdier.file_object.closed
+
+
+def test_mdfy_write_in_utf8():
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        contents = [
+            MdHeader("こんにちは")
+        ]
+        tmp_output_path = Path(tmp_dir, "output.md")
+        mdier = Mdfier(tmp_output_path)
+        with mdier as mdfier:
+            for content in contents:
+                mdfier.write(content)
+
+        with tmp_output_path.open(encoding="utf-8") as f:
+            lines = f.readlines()
+
+            assert lines[0] == "# こんにちは\n"
+            assert mdier.file_object.closed
