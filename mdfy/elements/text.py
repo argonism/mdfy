5b6a1af080b6f09f8abf26820d57cab4f3393938
argonism <44169535+argonism@users.noreply.github.com>
2025-03-31 00:37:57 +0900

    Make lark optional

--- mdfy/elements/text.py
+++ mdfy/elements/text.py
@@ -1,188 +1,14 @@
-import abc
-import re
-from typing import Dict, Optional
+from typing import Union
 
-from lark import Lark, Token, Tree
-from lark.visitors import Interpreter
+from mdfy.elements._base import MdElement
+from mdfy.elements.text_formatter import MdFormatter
 
-from ._base import MdElement
+try:
+    from mdfy.elements.formatter.lark_formatter import MdTextFormatter
 
-
-class MdTextInterpreter(Interpreter):
-    """Lark Interpreter tailored for MdText interpreting styled text.
-
-    Args:
-        style_patterns (Dict[str, str]): A dictionary mapping style names to formatting strings.
-    """
-
-    def __init__(self, style_patterns: Dict[str, str]):
-        super().__init__()
-        self.style_patterns = style_patterns
-
-    def process_content_tree(self, tree: Tree) -> str:
-        """Processes a subtree for content in yled text.
-
-        Args:
-            tree (Tree): The subtree to process.
-
-        Returns:
-            str: The processed text.
-        """
-        interpreted = ""
-        for child in tree.children:
-            if isinstance(child, Token):
-                interpreted += child.value
-            elif isinstance(child, Tree):
-                interpreted += self.styled_text(child)
-            else:
-                raise ValueError(f"Unable to handle this child type: {child.__class__}")
-        return interpreted
-
-    def styled_text(self, tree: Tree) -> str:
-        """Processes a subtree for styled text.
-
-        Args:
-            tree (Tree): The subtree to process.
-
-        Returns:
-            str: The styled text.
-        """
-        _, *content, _, style, _ = tree.children
-
-        target_text = ""
-        for tree in content:
-            target_text += self.process_content_tree(tree)
-
-        if not isinstance(style, Token):
-            raise ValueError(f"Expected style to be a Token, got {style.__class__}")
-
-        if style.value in self.style_patterns:
-            return self.style_patterns[style.value].format(target_text)
-        else:
-            return target_text
-
-    def start(self, tree: Tree) -> str:
-        return "".join(self.visit_children(tree))
-
-
-grammar = r"""
-    start: TEXT? styled_text? (start)* TEXT?
-    styled_text: LBRAK content+ COLON INTEXT RBRAK
-    content: INTEXT | INTEXT? styled_text
-    TEXT:   WS* /[^\[\]]+/ WS*
-    INTEXT: WS* /[^\[\]:]+/ WS*
-    LBRAK: "["
-    RBRAK: "]"
-    COLON: ":"
-
-    %import common.WS
-"""
-
-
-class MdFormatter(abc.ABC):
-    """Abstract base class for Markdown formatters."""
-
-    @abc.abstractmethod
-    def format(self, text: str) -> str:
-        """Formats the text and returns the formatted text.
-
-        Args:
-            text (str): The text to be formatted.
-
-        Returns:
-            str: The formatted text.
-        """
-        raise NotImplementedError
-
-
-class MdTextFormatter(MdFormatter):
-    """Markdown Text Formatter to handle text styling based on a specified grammar and style patterns.
-
-    Attributes:
-        grammar (str): The grammar to be used for parsing the text.
-        interpreter (Interpreter): The interpreter to be used for interpreting the parsed text.
-        patterns (Dict[str, str]): Expanded style patterns including aliases.
-    """
-
-    STYLE_PATTERNS = {
-        "strong": "***{}***",
-        "bold": "**{}**",
-        "italic": "*{}*",
-        "not": "~~{}~~",
-        "underline": "<u>{}</u>",
-        "code": "`{}`",
-    }
-
-    STYLE_ALIASES = {
-        "strong": ["st"],
-        "bold": ["bo", "bd"],
-        "italic": ["it"],
-        "not": ["no", "nt"],
-        "underline": ["un", "ul"],
-        "code": ["cd", "quote"],
-    }
-
-    def __init__(
-        self,
-        grammar: str = grammar,
-        interpreter: Optional[Interpreter] = None,
-        patterns: Optional[dict] = None,
-    ):
-        self.grammar = grammar
-        if self.grammar is None:
-            raise ValueError("Grammar cannot be None")
-        self.parser = Lark(self.grammar)
-
-        if patterns is None:
-            patterns = self.expand_style_patterns(
-                self.STYLE_PATTERNS, self.STYLE_ALIASES
-            )
-        self.patterns = patterns
-
-        if interpreter is None:
-            interpreter = MdTextInterpreter(self.patterns)
-        self.interpreter = interpreter
-
-    def expand_style_patterns(self, base_patterns: dict, aliases: dict) -> dict:
-        """Expands the style patterns with aliases.
-
-        Args:
-            base_patterns (Dict[str, str]): Base style patterns.
-            aliases (Dict[str, str]): Aliases for the style names.
-
-        Returns:
-            Dict[str, str]: Expanded style patterns.
-        """
-        expand_patterns = {
-            alias: base_patterns[style_name]
-            for style_name, aliases in aliases.items()
-            for alias in aliases
-        }
-        return {**base_patterns, **expand_patterns}
-
-    def format(self, text: str) -> str:
-        """Formats the text for style markers and returns the formatted text.
-
-        Args:
-            text (str): The text to be formatted.
-
-        Returns:
-            str: The formatted text.
-        """
-        parsed = self.parse(text)
-        result: str = self.interpreter.visit(parsed)
-        return result
-
-    def parse(self, text: str) -> Tree:
-        """Parses the text for style markers and returns the parsed text.
-
-        Args:
-            text (str): The text to be parsed.
-
-        Returns:
-            lark.Tree: The parsed syntax tree.
-        """
-        return self.parser.parse(text)
+    formatter_available = True
+except ImportError:
+    formatter_available = False
 
 
 class MdText(MdElement):
@@ -193,6 +19,8 @@ class MdText(MdElement):
         formatter (MdFormatter): The formatter to apply styling to the content.
 
     Examples:
+        >>> # If you have installed mdfy[styled-text]
+        >>> from mdfy import MdText
         >>> text = MdText("This is [bold:bold] text.")
         >>> print(text)
         This is **bold** text.
@@ -221,17 +49,28 @@ class MdText(MdElement):
             - code: cd, quote
     """
 
-    def __init__(self, content: str, formatter: MdFormatter = MdTextFormatter()):
+    def __init__(
+        self,
+        content: str,
+        formatter: Union[MdFormatter, None] = None,
+        no_style: bool = False,
+    ) -> None:
         """Initializes an instance of the MdText class to handle the text and styling of text.
 
         Args:
             content (str): The content string containing potential style markers.
             formatter (MdFormatter, optional): The formatter to apply styling to the content.
-                                                     Defaults to MdTextFormatter().
+                                                     Defaults to None.
+            no_style (bool, optional): If True, no style will be applied to the content.
+                                       Defaults to False.
         """
 
         self.content = content
         self.formatter = formatter
+        self.no_style = no_style
+
+        if self.formatter is None and formatter_available and not no_style:
+            self.formatter = MdTextFormatter()
 
     def __str__(self) -> str:
         """Returns the styled content as per the specified style markers.
@@ -240,7 +79,8 @@ class MdText(MdElement):
             str: Formatted markdown string with the appropriate styles applied.
         """
         result = self.content
-        result = self.formatter.format(result)
+        if self.formatter and not self.no_style:
+            result = self.formatter.format(result)
 
         return result
 
